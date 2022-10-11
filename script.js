a5fc6a930311b0398271def373f9f54fdf2339af
eulerspill <eulerspill@protonmail.com>
2022-10-12 00:04:42 +1000

    conform to new config template schema

--- script.js
+++ script.js
@@ -1,56 +1,74 @@
 import botiq from 'botiq';
+import fs from 'fs';
 
-export async function run(configs, compatability){
+const configs = JSON.parse(fs.readFileSync('./user-config.json'));
+const configTemplate = JSON.parse(fs.readFileSync('./config-template.json'));
+console.log(configs);
+await run(configs);
+
+export async function run(configs){
     let tokenTracker;
-    if (compatability === 'binance'){
-        const connection = await botiq.binance.createConnection({
+    const exchangeType = configs.exchangeType;
+    let binanceConnection;
+    if (exchangeType === 'binance'){
+        binanceConnection = await botiq.binance.createConnection({
             apiKey: configs.binanceAuth.apiKey,  
             apiSecret: configs.binanceAuth.apiSecret
         });
-        tokenTracker = await connection.createTracker({
-            tokenSymbol: configs.tokenPool.token, 
-            comparatorSymbol: configs.tokenPool.comparator
+        tokenTracker = await binanceConnection.createTracker({
+            tokenSymbol: configs.binanceTokenPool.token, 
+            comparatorSymbol: configs.binanceTokenPool.comparator
         });
-    } else {
+    } else if (exchangeType === 'evm'){
         const endpoint = await botiq.ethers.createJsonRpcEndpoint({
             accessURL: configs.jsonRpcEndpoint.url,
             rateLimitPerSecond: configs.jsonRpcEndpoint.rateLimitPerSecond
         });     
 
+        let comparatorAddress = configs.evmTokenPool.comparator;
+        const nativetokenRegex = /^[Nn][Aa][Tt][Ii][Vv][Ee]\s[Tt][Oo][Kk][Ee][Nn]$/;
+        if (nativetokenRegex.test(comparatorAddress)){
+            comparatorAddress = endpoint.nativeToken.address;
+        }
         tokenTracker = await endpoint.createTracker({
-            exchange: botiq.ethers.chains.ethereum.exchanges.uniswapV2,
-            tokenAddress: configs.tokenPool.token,
-            comparatorAddress: configs.tokenPool.comparator,
+            exchange: botiq.ethers.chains.ethereum.exchanges[configs.evmExchange],
+            tokenAddress: configs.evmTokenPool.token,
+            comparatorAddress,
         });
+    } else {
+        console.log(`Invalid exchange type "${exchangeType}"`);
+        return;
     }
 
+    const action = configs.orderAction;
+    const specifying =  action === 'buy' ? 'comparator' : 'token';
+    const amount = action === 'buy' ? configs.amountOfComparatorToSpend : configs.amountOfTokenToSell;
+
     const listenKey = tokenTracker.addSwapListener({listener: botiq.util.GENERIC_LOGGING_LISTENER});
-    const waitMethod = configs.order.action === 'buy' ? 'awaitPriceFall' : 'awaitPriceRise';
+    const waitMethod = action === 'buy' ? 'awaitPriceFall' : 'awaitPriceRise';
     await botiq.modules.awaitPriceMovement[waitMethod]({
         tracker: tokenTracker,
         triggerPriceString: configs.triggerPrice,
     });
     tokenTracker.removeListener({key: listenKey});
 
+    
     let actionResult;
-    if (compatability === 'binance'){
-        actionResult = await connection.swap({
+    if (configs.exchangeType === 'binance'){
+        actionResult = await binanceConnection.swap({
             tokenSymbol: tokenTracker.token.symbol, 
             comparatorSymbol: tokenTracker.comparator.symbol, 
-            action: configs.order.action,
-            amount: configs.order.amount,
-            specifying: configs.order.specifying
+            action, amount, specifying
         });
 
     } else {
         actionResult = await botiq.ethers.UniswapV2.swap({
             tracker: tokenTracker,
             privateKey: configs.privateWalletKey, 
-            action: configs.order.action,
-            amount: configs.order.amount,
-            specifying: configs.order.specifying,
-            slippagePercent: configs.order.slippage,
-            gasPercentModifier: configs.order.gasPercentModifier,
+            action, amount, specifying,
+            slippagePercent: configs.slippage,
+            justReturnEstimatedGasFee: true
+           //gasPercentModifier: configs.order.gasPercentModifier,
         });
     }
 
